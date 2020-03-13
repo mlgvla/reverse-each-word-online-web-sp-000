9e896ce2b6d4473da2229727181bd0cdb97c3abd
Monica Gerard <mlgerardvla@gmail.com>
2020-03-13 03:42:06 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  new_sentence = ""
+  word_array = string.split
+
+  word_array.each do |word|
+    new_sentence << word.reverse << " "    
+  end
+  return new_sentence
+end
