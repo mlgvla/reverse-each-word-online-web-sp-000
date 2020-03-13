3a655552389d404e3e86e7100045d89761b3cb7a
Monica Gerard <mlgerardvla@gmail.com>
2020-03-13 04:04:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,10 @@ def reverse_each_word(string)
   index = 0
   word_array.collect do |word|
     new_sentence << word.reverse
-    if index == word_array.length - 1
+    if index++ == word_array.length - 1
       new_sentence << ""
     else
       new_sentence << " "
     end
-    index += 1
   end
 end
